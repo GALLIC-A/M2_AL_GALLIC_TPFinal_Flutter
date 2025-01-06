4238b82061d2710e1bc50a5062da7623c6633f85
GALLIC-A <axel.gallic@orange.fr>
2025-01-06 18:48:19 +0100

    Etape 7 : Intégration de AuthService dans le Widget de Connexino

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -40,76 +40,78 @@ class _LoginScreenState extends State<LoginScreen> {
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text(widget.title),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Form(
-          key: _formKey, // Assignation de la clé au formulaire
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Form(
+            key: _formKey, // Assignation de la clé au formulaire
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
 // Champ Email
-              TextFormField(
-                controller: _emailController,
-                decoration: const InputDecoration(
-                  labelText: 'Email',
-                  border: OutlineInputBorder(),
-                  icon: Icon(Icons.mail),
-                ),
-//keyboardType: TextInputType.visiblePassword,
-                keyboardType: TextInputType.emailAddress,
-                validator: (value) {
-// Validation de l'email
-                  if (value == null || value.isEmpty) {
-                    return 'Veuillez entrer votre email';
-                  }
-// Regex simple pour valider l'email
-                  if (!RegExp(r'^[^@]+@[^@]+\.[^@]+').hasMatch(value)) {
-                    return 'Veuillez entrer un email valide';
-                  }
-                  return null;
-                },
-              ),
-              const SizedBox(height: 16.0),
-// Champ Mot de Passe
-              TextFormField(
-                controller: _passwordController,
-                decoration: const InputDecoration(
-                  labelText: 'Mot de passe',
-                  border: OutlineInputBorder(),
-                  icon: Icon(Icons.lock),
-                ),
-                obscureText: true, // Masquer le texte
-                validator: (value) {
-                  // Validation du mot de passe
-                  if (value == null || value.isEmpty) {
-                    return 'Veuillez entrer votre mot de passe';
-                  }
-                  if (value.length < 6) {
-                    return 'Le mot de passe doit contenir au moins 6 caractères';
-                  }
-                  return null;
-                },
-              ),
-              const SizedBox(height: 24.0),
-              // Bouton de Connexion OU indicateur de chargement
-              _isLoading
-                ? const CircularProgressIndicator()
-                : Column(
-                children: [
-                  ElevatedButton(
-                    onPressed: _login,
-                    child: const Text('Se connecter'),
+                TextFormField(
+                  controller: _emailController,
+                  decoration: const InputDecoration(
+                    labelText: 'Email',
+                    border: OutlineInputBorder(),
+                    icon: Icon(Icons.mail),
                   ),
-                  ElevatedButton(
-                      onPressed: _resetForm,
-                      child: const Text('Effacer le formulaire')
-                  )
-                ],
-              )
-            ],
+//keyboardType: TextInputType.visiblePassword,
+                  keyboardType: TextInputType.emailAddress,
+                  validator: (value) {
+// Validation de l'email
+                    if (value == null || value.isEmpty) {
+                      return 'Veuillez entrer votre email';
+                    }
+// Regex simple pour valider l'email
+                    if (!RegExp(r'^[^@]+@[^@]+\.[^@]+').hasMatch(value)) {
+                      return 'Veuillez entrer un email valide';
+                    }
+                    return null;
+                  },
+                ),
+                const SizedBox(height: 16.0),
+// Champ Mot de Passe
+                TextFormField(
+                  controller: _passwordController,
+                  decoration: const InputDecoration(
+                    labelText: 'Mot de passe',
+                    border: OutlineInputBorder(),
+                    icon: Icon(Icons.lock),
+                  ),
+                  obscureText: true, // Masquer le texte
+                  validator: (value) {
+                    // Validation du mot de passe
+                    if (value == null || value.isEmpty) {
+                      return 'Veuillez entrer votre mot de passe';
+                    }
+                    if (value.length < 6) {
+                      return 'Le mot de passe doit contenir au moins 6 caractères';
+                    }
+                    return null;
+                  },
+                ),
+                const SizedBox(height: 24.0),
+                // Bouton de Connexion OU indicateur de chargement
+                _isLoading
+                    ? const CircularProgressIndicator()
+                    : Column(
+                  children: [
+                    ElevatedButton(
+                      onPressed: _login,
+                      child: const Text('Se connecter'),
+                    ),
+                    ElevatedButton(
+                        onPressed: _resetForm,
+                        child: const Text('Effacer le formulaire')
+                    )
+                  ],
+                )
+              ],
+            ),
           ),
         ),
-      ),
+      )
     );
   }
 
@@ -118,44 +120,39 @@ class _LoginScreenState extends State<LoginScreen> {
       setState(() {
         _isLoading = true;
       });
-
       String email = _emailController.text;
       String password = _passwordController.text;
-
-      var user = await _authService.login(email, password);
-
+      final user = await _authService.login(email, password);
       setState(() {
         _isLoading = false;
       });
-
       if (user != null) {
 // Connexion réussie avec informations supplémentaires
         Navigator.push(
           context,
           MaterialPageRoute(
-            builder: (context) =>
-                ProfileScreen(
-                  email: email,
-                  firstName: _users[email]!['firstName'],
-                  lastName: _users[email]!['lastName'],
-                  role: _users[email]!['role'],
-                ),
+            builder: (context) => ProfileScreen(
+              email: email,
+              firstName: user['firstName'],
+              lastName: user['lastName'],
+              role: user['role'],
+            ),
           ),
         );
       } else {
 // Connexion échouée
         showDialog(
           context: context,
-          builder: (context) =>
-              AlertDialog(
-                title: Text('Erreur'),
-                content: Text('Email ou mot de passe incorrect'),actions: [
-                TextButton(
-                  onPressed: () => Navigator.pop(context),
-                  child: Text('OK'),
-                ),
-              ],
+          builder: (context) => AlertDialog(
+            title: Text('Erreur'),
+            content: Text('Email ou mot de passe incorrect'),
+            actions: [
+              TextButton(
+                onPressed: () => Navigator.pop(context),
+                child: Text('OK'),
               ),
+            ],
+          ),
         );
       }
     }
@@ -163,5 +160,7 @@ class _LoginScreenState extends State<LoginScreen> {
 
   void _resetForm() {
     _formKey.currentState!.reset();
+    _emailController.clear();
+    _passwordController.clear();
   }
 }
